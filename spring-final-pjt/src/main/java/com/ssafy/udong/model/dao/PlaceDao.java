55b4a38b648ff89fda35b4a261941baf4fdabf9a
yukyung-chung <kadie@naver.com>
2023-11-17 14:49:34 +0900

    Feat: Update calling API

--- spring-final-pjt/src/main/java/com/ssafy/udong/model/dao/PlaceDao.java
+++ spring-final-pjt/src/main/java/com/ssafy/udong/model/dao/PlaceDao.java
@@ -1,7 +1,12 @@
 package com.ssafy.udong.model.dao;
 
+import java.util.List;
+
 import com.ssafy.udong.model.dto.Place;
 
 public interface PlaceDao {
 	void insertPlace(Place place);
+
+	List<Place> getPlaces();
+
 }
