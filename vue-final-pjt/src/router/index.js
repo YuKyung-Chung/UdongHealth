32c9714ae8ed28e3614b7d1859333ad7288134d8
yhc-key <yhcho0712@gmail.com>
2023-11-17 16:42:36 +0900

    Feat: Search, placeDetail, SearchList

--- vue-final-pjt/src/router/index.js
+++ vue-final-pjt/src/router/index.js
@@ -6,6 +6,7 @@ import KindAll from '@/components/kind/KindAll.vue'
 import KindUpperBody from '@/components/kind/KindUpperBody.vue'
 import KindLowerBody from '@/components/kind/KindLowerBody.vue'
 import KindCore from '@/components/kind/KindCore.vue'
+import PlaceDetailView from '../views/PlaceDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,8 +27,12 @@ const router = createRouter({
     {
       path: '/search',
       name: 'search',
-      component: SearchView
-
+      component: SearchView,
+    },
+    {
+      path: '/placeDetail/:placeId',
+      name: 'placeDetail',
+      component: PlaceDetailView
     },
     {
       path: '/kind',
---
+++ vue-final-pjt/src/views/PlaceDetailView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>{{$route.params.placeId}}</h3>
+        <h4>잘넘어왔나요?</h4>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style  scoped>
+
+</style>
