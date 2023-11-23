018e5599907855b1707fb06571ecd66c763f1e50
yhc-key <yhcho0712@gmail.com>
2023-11-23 10:04:20 +0900

    Fix: FavList

--- vue-final-pjt/src/components/common/TheFavList.vue
+++ vue-final-pjt/src/components/common/TheFavList.vue
@@ -1,20 +1,26 @@
 <template>
-    <div v-if="userStore.loginTF === false" class="icon-text-container row" >
+    <div v-if="userStore.loginTF === false" class="icon-text-container row">
         <h4> 오늘의 운동 상식 </h4>
         <div v-for="art in arts">
-            
-            <p>{{ art.content}}</p>
+
+            <p>{{ art.content }}</p>
             <p class="writer">{{ art.writer }}</p>
         </div>
     </div>
     <div v-else>
-        <div v-if="favList.length ===0">
+        <div v-if="favList.length === 0">
             <p>아직 찜 목록이 없습니다.</p>
         </div>
-        <div v-else v-for="fav in favList">
-            <h4>{{ fav.placeId }}</h4>
-        </div>
+        <div v-else>
+            <h3> 즐겨찾는 장소</h3>
+            <div v-for="fav in favList" class="p-3 my-3 favBoard">
+                <RouterLink :to="`/placeDetail/${fav.placeId}`">
+                    <h4>{{ fav.addressGu }} {{ fav.addressDong }} {{ fav.공원명 }}</h4>
+                </RouterLink>
+                <p> {{ fav.설치운동기구종류 }}</p>
+            </div>
 
+        </div>
     </div>
 </template>
 
@@ -27,12 +33,12 @@ const userStore = useUserStore();
 const favList = ref([]);
 const user = ref({});
 const arts = [{
-    writer : "SSAFY",
-    content : "복식 호흡의 중요성 알고 계신가요?...",
+    writer: "SSAFY",
+    content: "복식 호흡의 중요성 알고 계신가요?...",
 
 }, {
-    writer : "조용환",
-    content : "무료 헬스장 100배 즐기기",
+    writer: "조용환",
+    content: "무료 헬스장 100배 즐기기",
 }]
 
 
@@ -44,6 +50,12 @@ watch([() => userStore.loginTF, () => userStore.addFavTF], async () => {
         try {
             const response = await axios.get(URL);
             favList.value = response.data;
+            favList.value.forEach((item) => {
+                const arr = item.설치운동기구종류.split(",");
+                if (item.설치운동기구종류.length >= 3) {
+                    item.설치운동기구종류 = `${arr[0]}, ${arr[1]}, ${arr[2]}, 등...`
+                }
+            })
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -72,11 +84,21 @@ watch([() => userStore.loginTF, () => userStore.addFavTF], async () => {
     display: flex;
     align-items: center;
 }
+
 p {
-    margin-bottom : 4px
+    margin-bottom: 4px
 }
+
 .writer {
-    font-weight : 700;
-    color : #1E88E5 
+    font-weight: 700;
+    color: #1E88E5
+}
+
+a {
+    text-decoration: none;
+}
+
+.favBoard {
+    border: 1px solid black
 }
 </style>
